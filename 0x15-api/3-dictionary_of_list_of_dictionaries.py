35a4d1037de4bc4528b0ce82da28560b4676cd5a
Arthur <arthmayer@outlook.fr>
2021-05-03 14:42:25 +0000

    add q3

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+using this REST API, for a given employee ID,
+returns information about his/her TODO list progress
+"""
+
+
+if __name__ == "__main__":
+    import json
+    import requests
+
+    
+    users_data = requests.get(
+            'https://jsonplaceholder.typicode.com/users').json()
+    json_dict = {}
+    
+    for user in users_data:
+        emp_id = user.get('id')
+        tasks = requests.get(
+                'https://jsonplaceholder.typicode.com/todos?userId={}'
+                     .format(emp_id)).json()
+        tasks_list = []
+
+        for task in tasks:
+            info = {"task": task.get('title'),
+                    "completed": task.get('completed'),
+                    "username": user.get('username')}
+
+            tasks_list.append(info)
+
+        json_dict[emp_id] = tasks_list
+
+    with open("todo_all_employees.json", mode="w") as emps_f:
+        json.dump(json_dict, emps_f)
